657c1502b863385956693c330a66e9636ca4440d
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-10 00:50:22 -0400

    Time: 21 ms (34.22%), Space: 36 MB (8.32%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int pivotIndex(vector<int>& nums) {
+    int bruteForceApproach(vector<int>& nums) {
         int n = nums.size();
         for (int i=0; i<n; i++) {
             int lsum = 0, rsum = 0, l = 0, r = n-1;
@@ -12,5 +12,29 @@ public:
                 return i;
         }
         return -1;
+    }
+
+    int prefixSumApproach(vector<int> &nums) {
+        int n = nums.size();
+        vector<int> lsum(n, 0);
+        vector<int> rsum(n, 0);
+
+        // lsum
+        for (int i = 1; i<n; i++)
+            lsum[i] = lsum[i-1] + nums[i-1];
+        
+        // rsum
+        for (int i = n-2; i>=0; i--)
+            rsum[i] = rsum[i+1] + nums[i+1];
+        
+        for (int i = 0; i < n; i++) {
+            if (lsum[i] == rsum[i])
+                return i;
+        }
+        return -1;
+    }
+
+    int pivotIndex(vector<int>& nums) {
+        return prefixSumApproach(nums);
     }
 };
