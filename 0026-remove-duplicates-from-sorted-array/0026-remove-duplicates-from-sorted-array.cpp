91cad9ecdae1f612b131b74fad42ad44a6a6b629
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-11 15:18:49 -0400

    Time: 7 ms (70.63%), Space: 21.2 MB (66.15%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicatesOptimized(vector<int>& nums) {
+        int dup = 1;
+        int in = 1;
+        while (dup < nums.size()) {
+            // Store the number
+            int unique = nums[dup];
+
+            if (nums[dup] != nums[dup - 1]) {
+                // Store the elem at the index in the nums array
+                nums[in++] = nums[dup];
+            }
+            dup++;
+        }
+        return in;
+    }
+
+    int removeDuplicatesWithWhileLoop(vector<int>& nums) {
         int dup = 0;
         int in = 0;
         while (dup < nums.size()) {
@@ -8,14 +24,17 @@ public:
             int unique = nums[dup];
 
             // Loop over the duplicate element
-            while (dup < nums.size() && nums[dup] == unique) {
+            while (dup < nums.size() && nums[dup] == unique)
                 dup++;
-            }
-            
+
             nums[in] = unique;
             in++;
         }
 
         return in;
+    }
+
+    int removeDuplicates(vector<int>& nums) {
+        return removeDuplicatesOptimized(nums);
     }
 };
