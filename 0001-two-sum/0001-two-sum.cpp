f41024b2b8a58cfe39d174d7e0284280c69886b4
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-11 10:51:14 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -13,7 +13,39 @@ public:
         }
         return ans;
     }
+
+    vector<int> twoSumTwoPointers(vector<int>& nums, int target) {
+        // Create a copy 
+        vector<int> nums_sorted(nums);
+
+        // sort the array
+        sort(nums_sorted.begin(), nums_sorted.end());
+        
+        vector<int> ans;
+
+        int l = 0;
+        int r = nums_sorted.size() - 1;
+        while (l < r) {
+            int sum = nums_sorted[l] + nums_sorted[r];
+            if (sum > target)
+                r--;
+            else if (sum < target)
+                l++;
+            else {
+                break;
+            }
+        }
+
+        for (int i=0; i < nums.size(); i++) {
+            if (nums[i] == nums_sorted[l])
+                ans.push_back(i);
+            else if (nums[i] == nums_sorted[r])
+                ans.push_back(r);
+        }
+        return ans;
+    }
+
     vector<int> twoSum(vector<int>& nums, int target) {
-        return twoSumBruteForce(nums, target);
+        return twoSumTwoPointers(nums, target);
     }
 };
