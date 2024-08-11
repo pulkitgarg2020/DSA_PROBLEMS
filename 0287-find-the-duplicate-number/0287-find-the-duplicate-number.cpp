9a1db9fbf76b365d9882234517120f180bfc5f98
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-11 15:45:20 -0400

    Time: 119 ms (20.52%), Space: 65 MB (31.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+    int usingSort(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        cout << "NUMS I: " << nums[0] << " NUMS I-1: " << nums[1]; 
         for (int i=1; i<=nums.size()-1; i++) {
             if (nums[i] == nums[i-1])
                 return nums[i];
         }
         return 0;
+    }
+
+    int findDuplicate(vector<int>& nums) {
+        return usingSort(nums);
     }
 };
