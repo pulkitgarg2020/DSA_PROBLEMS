203b4223d7ee18d7deb9c07d5e8a047f43eff014
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-12 22:55:11 -0400

    Time: 39 ms (63.05%), Space: 36.1 MB (78.37%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -32,9 +32,9 @@ public:
                 i++;
         }
 
-        for (int i=0; i<n; i++) {
-            if (nums[i] != i+1)
-                ans.push_back(i+1);
+        for (int i=1; i<=n; i++) {
+            if (nums[i-1] != i)
+                ans.push_back(i);
         }
 
         return ans;
