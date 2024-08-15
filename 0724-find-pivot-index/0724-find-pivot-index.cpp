900ab0b31140399f57a6f4f18435fa966fcbe36d
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-15 12:59:40 -0400

    Time: 9 ms (94.37%), Space: 36 MB (9.27%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    int bruteForceApproach(vector<int>& nums) {
-        int n = nums.size();
-        for (int i=0; i<n; i++) {
-            int lsum = 0, rsum = 0, l = 0, r = n-1;
-            while (l < i)
-                lsum = lsum + nums[l++];
-            while (r > i)
-                rsum = rsum + nums[r--];
-            if (lsum == rsum)
-                return i;
-        }
-        return -1;
-    }
+    int findSumUsingVectors(vector<int>& nums) {
+        vector<int> lsum(nums.size(), 0);
+        vector<int> rsum(nums.size(), 0);
 
-    int prefixSumApproach(vector<int> &nums) {
         int n = nums.size();
-        vector<int> lsum(n, 0);
-        vector<int> rsum(n, 0);
-
-        // lsum
-        for (int i = 1; i<n; i++)
+        int r = nums.size()-2;
+        for (int i=1; i<n; i++)
             lsum[i] = lsum[i-1] + nums[i-1];
-        
-        // rsum
-        for (int i = n-2; i>=0; i--)
+        for (int i=n-2; i>=0; i--)
             rsum[i] = rsum[i+1] + nums[i+1];
         
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i<nums.size(); i++) {
             if (lsum[i] == rsum[i])
                 return i;
         }
+
         return -1;
     }
 
     int pivotIndex(vector<int>& nums) {
-        return prefixSumApproach(nums);
+        return findSumUsingVectors(nums);
     }
 };
