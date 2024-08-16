699b6521d07383d4d89b03b53091397eae83d224
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-16 17:02:17 -0400

    Time: 3 ms (38.49%), Space: 8.5 MB (21.11%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -9,7 +9,7 @@ public:
             long m = s + (e-s)/2;
             if (m * divisor == dividend)
                return m;
-            if (m * divisor < dividend) {
+            else if (m * divisor < dividend) {
                 ans = m;
                 s = m + 1;
             } else e = m - 1;
@@ -22,17 +22,11 @@ public:
         if (dividend == INT_MIN && divisor == 1) return INT_MIN;
 
         long quot = getQuotient(abs(dividend), abs(divisor));
-        cout << "Quot: " << quot << endl;
         int ans = quot;
         bool cond = dividend<0;
-        cout << "Dividend: " << dividend << " Cond: " << cond << endl;
-        cout << "Divisor: " << divisor << endl;
 
         if (dividend < 0 && divisor < 0) return ans;
         else if (dividend < 0 || divisor < 0) return -(ans);
-
-        // if ((divisor > 0 && dividend < 0) || (divisor < 0 && dividend > 0))
-        //     return -ans;
         
         return ans;
     }
