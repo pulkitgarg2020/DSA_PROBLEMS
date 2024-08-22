2149da9a5f46f3774ba32a2edb0665f93f154bc1
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-22 11:59:02 -0400

    Time: 24 ms (86.21%), Space: 33.7 MB (93.69%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
 
+    int lowerBound(vector<int> &a, int x) {
+        int s = 0;
+        int e = a.size() - 1;
+        int ans = e;
+        while (s<=e) {
+            int m = s+(e-s)/2;
+            if (a[m] >= x) {
+                ans=m;
+                e = m-1;
+            } else if (x>a[m]) s = m+1;
+            else e = m-1;
+        }
+        return ans;
+    }
+
+    vector<int> binarySearchMethod(vector<int>& arr, int k, int x) {
+        int h = lowerBound(arr, x);
+        int l = h-1;
+        while (k--) {
+            if (l < 0) h++;
+            else if (h >= arr.size()) l--;
+            else if (x-arr[l] > arr[h]-x) h++;
+            else l--;
+        }
+        // low and high is not included
+        return vector<int>(arr.begin()+l+1, arr.begin()+h);
+    }
+
     vector<int> twoPointerApproach(vector<int>& arr, int k, int x) {
         int l = 0;
         int h = arr.size() - 1;
@@ -12,7 +40,8 @@ public:
 
         return vector<int>(arr.begin()+l, arr.begin()+h+1);
     }
+
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPointerApproach(arr, k, x);
+        return binarySearchMethod(arr, k, x);
     }
 };
