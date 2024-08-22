57d0a1d639bf7f1feea965077ec7911282ef3812
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-22 00:32:01 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+
+    int binarySearch(vector<int>& a, int x) {
+        int s = 0;
+        int e = a.size() - 1;
+
+        while (s <= e) {
+            int m = s + (e-s)/2;
+            if (a[m] == x) return m;
+            else if (a[m] > x) e = m-1;
+            else s = m+1;
+        }
+
+        return -1;
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans;
+        
+        // if the length of the vector is 
+        // equal to k then return the 
+        // vector
+        if (k == arr.size()) return arr;
+        int s=0, e=arr.size()-1;
+
+        int c_start;
+        int c_end;
+
+        int exist = binarySearch(arr, x);
+
+        // If element is not in the array
+        if (exist == -1) {
+            if (arr[e] < x) {
+                // if element in not in array
+                // and is bigger than the last
+                // elem that means the size - k
+                // se shuru hoke sare uske baad
+                // closest elements hai
+                c_start = arr.size() - k;
+                c_end = arr.size();
+            } else if (arr[s] > x) {
+                // if element in not in arry and is smaller
+                // than the first element, then the closest
+                // elements start se leke k tak honge
+                c_start = 0;
+                c_end = k;
+            }
+        } else {
+            // Element exists in the array
+            
+            // because we found one element in the arry
+            int l, r;
+            
+            l = exist-1;
+            r = exist+1;
+
+            while(k>1) {
+                if (l >= 0 && (r >= arr.size() || abs(x - arr[l]) <= abs(arr[r] - x))) l--;
+                else r++;
+                k--;
+            }
+
+            c_start = l + 1;
+            c_end = r;
+        }
+
+        for (int i=c_start; i<c_end; i++) {
+            ans.push_back(arr[i]);
+        }
+
+        sort(ans.begin(), ans.end()); 
+        return ans;
+    }
+};
