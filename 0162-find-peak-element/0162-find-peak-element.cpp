f2c663ca9b58d262b35bc50ae8fc7781e736154c
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-08-22 22:20:26 -0400

    Time: 3 ms (64.67%), Space: 11.5 MB (61.01%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) {
+        int s = 0;
+        int e = a.size() - 1;
+        
+        while (s < e) {
+            int m = s + (e - s) / 2;
+            cout << "M: " << m << endl;
+            
+            if (a[m] > a[m + 1]) {
+                e = m;  // Move towards the peak on the left
+            } else {
+                s = m + 1;  // Move towards the peak on the right
+            }
+        }
+        
+        return s;  // s == e, and this is the peak
+    }
+};
