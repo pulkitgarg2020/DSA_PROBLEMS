4be1485ed762abfcc3dbe3646701e8e0acb72ec8
pulkitgarg2020 <pl201920@dal.ca>
2024-08-25 11:57:32 -0400

    Square root with precision

---
+++ Sqrt-with-precision/README.md
@@ -0,0 +1,15 @@
+<h2> Find square root of number upto given precision using binary search </h2>
+
+<p> Given a positive number n and precision p, find the square root of number upto p decimal places using binary search. </p>
+
+'''
+Input : number = 50, precision = 3
+Output : 7.071
+
+Input : number = 10, precision = 4
+Output : 3.1622
+'''
+
+<p> Time Complexity : The time required to compute the integral part is O(log(number)) and constant i.e, = precision for computing the fractional part. Therefore, overall time complexity is O(log(number) + precision) which is approximately equal to O(log(number)).
+
+Auxiliary Space: O(1) since it is using constant space for variables </p>
---
+++ Sqrt-with-precision/sqrt_with_precision.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    int findSqrt(int num)
+    {
+        int s = 0;
+        int e = num;
+        int sqrt = 0;
+        while (s <= e)
+        {
+            double m = s + (e - s) / 2;
+            if (m * m == num)
+                return m;
+            else if (m * m > num)
+                e = m - 1;
+            else
+            {
+                sqrt = m;
+                s = m + 1;
+            }
+        }
+        return sqrt;
+    }
+
+    double findSqrtPrecision(int num, int p)
+    {
+        int sqrt = findSqrt(num);
+        double sqrt_precision = sqrt;
+        p = -p;
+        for (double i = sqrt_precision; i <= num; i = i + pow(10, p))
+        {
+            if (i * i < num)
+                sqrt_precision = i;
+            else
+                break;
+        }
+        return sqrt_precision;
+    }
+}
