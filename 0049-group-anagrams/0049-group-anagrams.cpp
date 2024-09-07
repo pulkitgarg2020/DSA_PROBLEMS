dd1ae78221e6f7f8fdbecc3d344a839d92de1fb1
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-07 00:09:21 -0400

    Time: 23 ms (83.58%), Space: 25.1 MB (60%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,51 +1,13 @@
 class Solution {
 public:
-
-    // bool isAnagram(string s, string t) {
-    //     int i = 0;
-    //     while(i < s.size()) {
-    //         char c = s[i++];
-    //         int p = t.find(c);
-    //         if (p != string::npos) t.erase(p, 1);
-    //         else return false;
-    //     }
-    //     return true;
-    // }
-
-    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    //     std::set<vector<string>> ans;
-    //     for (int i = 0; i<strs.size(); i++) {
-    //         set<string> group;
-    //         group.insert(strs[i]);
-    //         for (int j=i+1; j<strs.size(); j++) {
-    //             if (isAnagram(strs[i], strs[j])) group.insert(strs[j]);
-    //         }
-    //         std::vector<string> groups(group.begin(), group.end());
-    //         ans.insert(groups);
-    //     }
-    //     std::vector<vector<string>> anagrams(ans.begin(), ans.end());
-    //     return anagrams;
-    // }
-
-    void print(vector<string>& strs) {
-        for(auto i: strs) cout << i << endl;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // store strs in temp
-        vector<string> temp(strs);
+        std::unordered_map<string, vector<string>> map;
 
         for (int i = 0; i<strs.size(); i++) {
             string str = strs[i];
             sort(str.begin(), str.end());
-            temp[i] = str;
+            map[str].push_back(strs[i]);
         }
-
-        std::unordered_map<string, vector<string>> map;
-
-        // add string to map, using sorted string as key
-        for (int i = 0; i<strs.size(); i++) 
-            map[temp[i]].push_back(strs[i]);
 
         vector<vector<string>> ans;
 
