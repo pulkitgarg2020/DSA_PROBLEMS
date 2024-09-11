be82d71f311ab6ef474ce3434b159957a49095e8
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-10 22:12:00 -0400

    Time: 7 ms (26.44%), Space: 12.8 MB (91.66%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int count = 1;
+        string s;
+        for (int i = 0; i < chars.size(); i++) {
+            // store the current char
+            char curr = chars[i];
+
+            cout << "I: " << i << endl;
+            cout << "Current: " << curr << endl;
+
+            // push the current char in array
+            s.push_back(curr);
+            while (i < chars.size()-1 && chars[i+1] == curr) {
+                i++;
+                count++;
+            }
+
+            cout << "Count: " << count << endl;
+            if (count > 1) {
+                if (count >= 10) s.append(std::to_string(count));
+                else s.push_back(count + '0');
+            }
+            count = 1;
+        }
+        chars = std::vector(s.begin(), s.end());
+        return s.length();
+    }
+};
+
+/*
+
+    int compress(vector<char>& chars) {
+        vector<char> g(280, 0);
+
+        for (auto c: chars) g[c]++;
+
+        string s;
+
+        for (int i=0; i<g.size(); i++) {
+            if (g[i] > 0) {
+                char c = i;
+                s.push_back(c);
+                int freq = g[i];
+                if (freq > 1) {
+                    if (freq > 10) s.append(std::to_string(freq));
+                    else s.push_back(freq + '0');
+                }
+            }
+        }
+        cout << s << endl;
+        chars = std::vector(s.begin(), s.end());
+        return s.length();
+    }
+
+*/
