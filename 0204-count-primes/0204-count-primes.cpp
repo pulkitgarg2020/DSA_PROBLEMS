280493947a386ad20bc34c41ee5d735f1efcbab5
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-10 23:57:40 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countPrimes(int n) {
+    int countPrimesSieve(int n) {
         if (n == 0) return 0;
         vector<bool> prime(n, true);
         prime[0] = prime[1] = false;
@@ -18,5 +18,32 @@ public:
             }
         }
         return ans;
+    }
+
+    int countPrimesSieveOptimized(int n) {
+        if (n == 0) return 0;
+        vector<bool> prime(n, true);
+        prime[0] = prime[1] = false;
+        int ans = 0;
+
+        for (int i=2; i<n; i++) {
+            if (prime[i]) {
+                ans++;
+
+                // int j = 2*i;
+                long long j = i * i; // Optimization 1
+                // first unmarked number would be i*i as others 
+                // have been marked by 2 to (i-1)
+                while (j < n) {
+                    prime[j] = false;
+                    j+=i;
+                }
+            }
+        }
+        return ans;
+    }
+
+    int countPrimes(int n) {
+        return countPrimesSieveOptimized(n);
     }
 };
