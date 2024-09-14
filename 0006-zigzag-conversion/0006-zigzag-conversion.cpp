c6fe1ac9711ddd714517302bb7d94d765f291cc2
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-14 18:38:51 -0400

    Time: 12 ms (55.78%), Space: 13.9 MB (46.26%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -7,18 +7,14 @@ public:
         int dir = 0;
         int i = 0;
         while (i < s.length()) {
-            cout << "DIR in start: " << dir << endl;
             while(i < s.length() && dir <= n-1) {
                 char c = s[i];
-                cout << "dir down: " << dir << " c: " << c << endl;
                 rows[dir++] += c; 
                 i++;
             }
-            cout << "DIR: " << dir << endl;
             dir = dir-2;
             while(i < s.length() && dir > 0) {
                 char c = s[i];
-                cout << "dir up: " << dir << " c: " << c << endl;
                 rows[dir--] += c;
                 i++;
             }
