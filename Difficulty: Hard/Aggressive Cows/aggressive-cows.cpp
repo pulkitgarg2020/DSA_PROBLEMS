148a5790724f10e36674fc137009d445f135e13a
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-14 22:44:52 -0400

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Hard/Aggressive Cows/aggressive-cows.cpp
@@ -9,38 +9,43 @@ using namespace std;
 
 class Solution {
 public:
+
     bool possibleSolution(vector<int> &stalls, int cows, int min) {
-        // can we place k cows with at least min distance 
-        // between cows
-        
         int c = 1;
-        int pos = stalls[0];
         
-        for (int i=1; i<stalls.size(); i++) {
+        int pos = stalls[0]; // first cow ko place kardo
+        
+        for (int i = 1; i < stalls.size(); i++) {
             if (stalls[i] - pos >= min) {
                 c++;
-                pos = stalls[i]; // one more cow has been placed
+                pos = stalls[i];
             }
-            
-            if (c==cows) return true;
+            if (c == cows) return true;
         }
+        
         return false;
     }
-    
+
     int solve(int n, int k, vector<int> &stalls) {
         // Write your code here
         sort(stalls.begin(), stalls.end());
+        int ans;
         int s = 0;
+        
+        // biggest element - smallest element
         int e = stalls[n-1] - stalls[0];
-        int ans = -1;
+        
         while (s <= e) {
             int m = s + (e-s)/2;
             if (possibleSolution(stalls, k, m)) {
                 ans = m;
                 s = m+1;
-            } else e = m-1;
+            } else {
+                e = m - 1;
+            }
         }
         return ans;
+        
     }
 };
 
