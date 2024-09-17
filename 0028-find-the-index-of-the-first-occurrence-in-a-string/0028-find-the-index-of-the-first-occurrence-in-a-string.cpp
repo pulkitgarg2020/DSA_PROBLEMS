7d5accdfd79c93030bf63442d155bb5351c63be0
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-17 13:44:18 -0400

    Time: 0 ms (100%), Space: 7.9 MB (64.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,13 +5,8 @@ public:
         for (int i=0; i<haystack.length(); i++) {
             int j = 0;
             int index = i;
-            // cout << "I: " << i << endl;
-            // cout << "hay: " << haystack[i] << endl;
-            // cout << "Needle: " << needle[j] << endl;
-            // cout << "Index: " << index << endl;
-            while (i < haystack.length() && haystack[i] == needle[j]) {
-                cout << "I inside: " << i << endl;
-                i++;
+            int k = i;
+            while (i < haystack.length() && haystack[k++] == needle[j]) {
                 j++;
                 if (j == needle.length()) return index;
             }
