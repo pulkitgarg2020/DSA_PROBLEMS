78410163f60af645b95438d5a9c858407d2a67c1
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-09-20 13:49:06 -0400

    Time: 3 ms (81.2%), Space: 8.4 MB (93.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,31 +3,18 @@ public:
     string reverseWords(string s) {
     reverse(s.begin(), s.end()); 
 	int i = 0;
-    int start, end;
-    while (i < s.length() && s[i] != '\0') {
-        start = i;
-        while (i < s.length() && s[i] != ' ') i++;
-        end = i;
-        cout << "Start: " << start << " End: " << end << endl;
-        reverse(s.begin()+start, s.begin()+end);
+    int n = s.length();
+    int l = 0, r = 0;
+    while (i < n) {
+        while (i < s.length() && s[i] == ' ') i++;
+        if (i == n) break;
+        while (i < s.length() && s[i] != ' ') s[r++] = s[i++];
+        reverse(s.begin()+l, s.begin()+r);
+        s[r++] = ' ';
+        l = r;
         i++;
     }
-
-        int j = 0; // This pointer will build the final cleaned-up string
-
-        for (int i = 0; i < s.length(); i++) {
-            // Skip leading spaces
-            if (s[i] == ' ' && (j == 0 || s[j - 1] == ' ')) continue;
-            // Copy character to the new position
-            s[j++] = s[i];
-        }
-
-        // Remove trailing space if exists
-        if (j > 0 && s[j - 1] == ' ') j--;
-
-        // Resize the string to the new cleaned size
-        s.resize(j);    
-
+    s.resize(r-1);    
     return s;
     }
 };
