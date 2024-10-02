41e6301971d244ec19ca389d5213423e74f02f75
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-10-02 15:04:49 -0400

    Time: 132 ms (26.32%), Space: 61.8 MB (57.16%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,34 +1,56 @@
 class Solution {
 public:
-    bool possibleSolution(vector<int>& p, int balls, int mid) {
-        int c = 1;
-        int pos = p[0];
-        for (int i=1; i<p.size(); i++) {
-            if (p[i]-pos >= mid) {
-                c++;
-                pos = p[i];
+    bool possibleSolution(int d, vector<int>& p, int numBall) {
+        // assume first ball is placed at first position
+        int first = p[0];
 
-                if (c==balls) return true;
+        // agar main ball rakhta hoon 2nd, distance b/w 1st and
+        // 2nd ball >= distance
+        // if this is true, ball rakhdo
+        // phr next ball ko dekho
+        int ball = 1;
+        int last = p[0];
+        for (int i=1; i<p.size(); i++) {
+            int dis = p[i] - last;
+
+            if (dis >=  d) {
+                ball++;
+                last = p[i];
+                if (ball == numBall) return true;
             }
         }
         return false;
     }
 
-    int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(), position.end());
+    int binarySearch(vector<int>& p, int numBall) {
+        // search space define karta hai 
+        int n = p.size()-1;
         int s = 0;
-        int n = position.size();
-        int e = position[n-1] - position[0];
-        int ans = -1;
+        int e = p[n] - p[0];
+        int ans = 0;
         while (s <= e) {
             int mid = s + (e-s)/2;
-            if (possibleSolution(position, m, mid)) {
+            if (possibleSolution(mid, p, numBall)) {
                 ans = mid;
-                s = mid+1;
-            } else {
-                e = mid-1;
+                s = mid + 1;
             }
+            else e = mid - 1;
         }
         return ans;
     }
+
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(), position.end());
+        int ans = binarySearch(position, m);
+        return ans;
+    }
 };
+
+/*
+1 3 7 == 2, 4 ==> 2
+1 3 4 == 2, 1 ==> 1
+2 4 7 == 2, 3 ==> 2
+1 4 7 == 3, 3 ==> 3
+
+1 2 3 4 7 
+*/
