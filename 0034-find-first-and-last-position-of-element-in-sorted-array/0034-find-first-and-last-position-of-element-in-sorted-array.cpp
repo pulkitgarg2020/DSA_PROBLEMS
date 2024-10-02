5b3c256cdbea69b218e86987903708bffe68f899
pulkitgarg2020 <66171179+pulkitgarg2020@users.noreply.github.com>
2024-10-02 17:16:37 -0400

    Time: 8 ms (20.91%), Space: 16.3 MB (41.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
 
-    int findRange(vector<int>& nums, int target, bool range) {    
-        int first = -1;   
+    // find last element
+    int findFirst(vector<int>& nums, int t) {
+        int first = -1;
         int s = 0;
         int e = nums.size() - 1;
 
         while (s <= e) {
-            int m = (s+e)/2;
-            if (nums[m] == target) {
-                first = m;
-                if (range)
-                    e = m-1;
-                else 
-                    s = m+1;
-            } else if (target > nums[m])
-                s = m+1;
-            else 
-                e = m-1;
-        }
+            int m = s + (e-s)/2;
 
+            if (nums[m] > t) e = m - 1;
+            else if (nums[m] < t) s = m+1;
+            else {
+                first = m;
+                e = m-1;
+            }
+        }
         return first;
     }
 
+    int findLast(vector<int>& nums, int t) {
+        int last = -1;
+        int s = 0;
+        int e = nums.size() - 1;
+
+        while (s <= e) {
+            int m = s + (e-s)/2;
+
+            if (nums[m] > t) e = m - 1;
+            else if (nums[m] < t) s = m+1;
+            else {
+                last = m;
+                s = m+1;
+            }
+        }
+        return last;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findRange(nums, target, true);
-        int last = findRange(nums, target, false);
-        vector<int> ans;
-        ans.push_back(first);
-        ans.push_back(last);
-        return ans;
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        // cout << first << endl;
+        // cout << last << endl;
+        if (first <= last) return {first, last};
+        else return {-1, -1};
     }
 };
